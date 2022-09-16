9c223923e8b2ef62b3c1937a2f956b8de3900439
OwgerD <113381416+OwgerD@users.noreply.github.com>
2022-09-16 14:38:19 -0400

    I made section one and two
    I used backslashes, slashes, and up-lines in order to create the smiley face, using spaces to align the lines. Then, I went online to look for a text art image for inspiration, before trying to re-create the image without copy-pasting, I failed at perfectly re-creating it, but it came out well regardless.

--- ProblemSet2_1.java
+++ ProblemSet2_1.java
@@ -2,12 +2,23 @@ public class ProblemSet2_1 {
     public static void main(String[] args) {
         //Use 8 print statements to print a smiley face.
         //Your art will rely on only a single character, besides space, such as X or #.
+        System.out.println("  ____  ");
+        System.out.println(" /    \\ ");
+        System.out.println("| |  | |");
+        System.out.println("|      |");
+        System.out.println("| \\__/ |");
+        System.out.println(" \\____/");
         
         
         
         
-        
-        
+        System.out.println("  __________  ");
+        System.out.println(" / ___   ___\\");
+        System.out.println("/ /@@@\\ /@@@\\\\");
+        System.out.println("\\ \\@@@/ \\@@@//");
+        System.out.println(" \\___  ''___/");
+        System.out.println("     | _ |");
+        System.out.println("      \\_/");
         
         
     }   
